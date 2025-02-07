61fbb41ed75a944b542a9b3b1e8aee6f20e1b743
Octavio Rivera <52300221+TavoRivera@users.noreply.github.com>
2025-02-07 20:04:02 +0000

    pruebas a ambiente de produccion 3

--- phoenixsolution/phoenixsolution/settings.py
+++ phoenixsolution/phoenixsolution/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = "django-insecure-0qx1e2z1-lfvd*axbg88tlg!a$zv04uzjqkz+2q&7m1d(6npih
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CSRF_TRUSTED_ORIGINS = ['https://localhost:8000', 'http://127.0.0.1:8000',   "https://urban-space-enigma-5xwg5x7g9gx2vqwp-8000.app.github.dev","phoenix-solution-webpage.onrender.com"]
+CSRF_TRUSTED_ORIGINS = [
+    'https://localhost:8000',
+    'http://127.0.0.1:8000',
+    "https://urban-space-enigma-5xwg5x7g9gx2vqwp-8000.app.github.dev",
+    "https://phoenix-solution-webpage.onrender.com"  # Agregar https:// aquí
+]
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost","urban-space-enigma-5xwg5x7g9gx2vqwp-8000.app.github.dev","https://phoenix-solution-webpage.onrender.com"]
 
