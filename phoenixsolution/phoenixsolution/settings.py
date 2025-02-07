58d7c587510632a6e7e65bec2b2d45c23a1cabd3
Octavio Rivera <52300221+TavoRivera@users.noreply.github.com>
2025-02-07 19:58:16 +0000

    pruebas a ambiente de produccion 2

--- phoenixsolution/phoenixsolution/settings.py
+++ phoenixsolution/phoenixsolution/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = ['https://localhost:8000', 'http://127.0.0.1:8000',   "https://urban-space-enigma-5xwg5x7g9gx2vqwp-8000.app.github.dev","phoenix-solution-webpage.onrender.com"]
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost","urban-space-enigma-5xwg5x7g9gx2vqwp-8000.app.github.dev","phoenix-solution-webpage.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost","urban-space-enigma-5xwg5x7g9gx2vqwp-8000.app.github.dev","https://phoenix-solution-webpage.onrender.com"]
 
 
 # Application definition
